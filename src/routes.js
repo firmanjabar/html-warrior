21e96dba3203619add15e358abce8f2654f1b703
FirmanJabar <firmanabduljabar@gmail.com>
2019-10-08 22:29:31 +0800

    adding header link when login, logout, guard, refresh token and fixing route guard

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,54 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from './Store/store'
 
 import Home from './components/Home/Index.vue'
 import SignIn from './components/Signin/Index.vue'
+import Dashboard from './components/Dashboard/Index.vue'
 
 Vue.use(VueRouter)
+
+const authGuard = {
+    beforeEnter(to, from, next) {
+
+        const redirect = () => {
+            if (store.state.admin.token) {
+                if (to.path === '/signin') {
+                    next('/dashboard')
+                } else {
+                    next()
+                }
+            } else {
+                if (to.path === '/signin') {
+                    next()
+                } else {
+                    next('/')
+                }
+            }
+        }
+
+        if (store.state.admin.refreshLoading) {
+            store.watch((state, getters) => getters['admin/refreshLoading'], () => {
+                redirect()
+            })
+        } else {
+            redirect()
+        }
+    }
+}
 
 const routes = [{
     path: '/',
     component: Home
 }, {
     path: '/signin',
-    component: SignIn
+    component: SignIn,
+    ...authGuard
+}, {
+    path: '/dashboard',
+    component: Dashboard,
+    children: [],
+    ...authGuard
 }]
 
 export default new VueRouter({

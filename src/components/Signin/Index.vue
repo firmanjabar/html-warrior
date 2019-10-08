3a957d2dfe8a2ea329e0875fb3060d93da458d2a
FirmanJabar <firmanabduljabar@gmail.com>
2019-10-08 14:55:54 +0800

    adding login feature from firebase auth

---
+++ src/components/Signin/Index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+    <div class="signin_container">
+      <h2>Sign In</h2>
+
+      <form @submit.prevent="onSubmit">
+        <div class="input_field" :class="{invalid: $v.formdata.email.$error}">
+          <label>Email</label>
+          <input type="email" @blur="$v.formdata.email.$touch()" v-model.trim="formdata.email" />
+          <div v-if="$v.formdata.email.$error">
+            <p class="error_label" v-if="!$v.formdata.email.required">This field is required</p>
+            <p class="error_label" v-if="!$v.formdata.email.email">Please enter a valid email</p>
+          </div>
+        </div>
+        <div class="input_field" :class="{invalid: $v.formdata.password.$error}">
+          <label>Password</label>
+          <input
+            type="password"
+            @blur="$v.formdata.password.$touch()"
+            v-model.trim="formdata.password"
+          />
+          <div v-if="$v.formdata.password.$error">
+            <p class="error_label" v-if="!$v.formdata.password.required">This field is required</p>
+            <p class="error_label" v-if="!$v.formdata.password.minLength">At least 6 characters</p>
+          </div>
+        </div>
+        <button class="btnfull" type="submit">Sign In</button>
+        <p class="error_label" v-if="error">Something went wrong!</p>
+        <p class="error_label" v-if="authFailed">Please check your email or password!</p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required, email, minLength } from "vuelidate/lib/validators";
+
+export default {
+  data() {
+    return {
+      error: false,
+      formdata: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  computed: {
+    authFailed() {
+      return this.$store.state.admin.authFailed;
+    }
+  },
+  destroyed() {
+    this.$store.commit("admin/authFailed", "reset");
+  },
+  validations: {
+    formdata: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (!this.$v.$invalid) {
+        this.$store.dispatch("admin/signIn", this.formdata);
+      } else {
+        this.error = true;
+        setTimeout(() => {
+          this.error = false;
+        }, 2000);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.input_field.invalid input,
+.input_field.invalid input {
+  border: 2px solid red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+
 import router from './routes'
+import store from './Store/store'
 
 import BootstrapVue from "bootstrap-vue"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuelidate from 'vuelidate'
 
 import feather from 'vue-icon'
 
 import Buttons from './components/UI/Buttons.vue'
 
+// Material Design
 Vue.use(feather, 'v-icon')
 Vue.use(BootstrapVue)
 Vue.component('app-button', Buttons)
 
+// MISC
+Vue.use(vuelidate)
+
+// Resource
+Vue.use(VueResource)
+Vue.http.options.root = ''
+
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
